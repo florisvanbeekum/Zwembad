4e02f62920dc8dc4a3717a28007c41e2319bc7d2
florisvanbeekum <fmvanbeekum@gmail.com>
2018-07-29 16:15:19 +0200

    2 functies aan drukknop gegeven
    als je de knop kort indrukt toggled de pomp voor 60 seconden. als je de knop lang indrukt dan toggled de pomp voor 5 uur.

--- zwembad2/zwembad2.ino
+++ zwembad2/zwembad2.ino
@@ -25,6 +25,20 @@ LiquidCrystal_I2C lcd(0x38, 16, 2);
 int PompPin = 3;
 int ButtonPin = 5;
 
+boolean buttonActive = false;
+boolean longPressActive = false;
+long buttonTimer = 0;
+long longPressTime = 500;
+
+boolean ShortOverRule = false;
+unsigned long ShortOverRuleTimer;
+long ShortOverRuleTime = 60000;
+
+boolean LongOverRule = false;
+unsigned long LongOverRuleTimer;
+long LongOverRuleTime = 18000000;
+
+
 
 float TempFlowOut;
 float TempPool;
@@ -51,19 +65,22 @@ float TempDelta = 5.00;
 float MaxTempPool = 24.00;
 
 
-int TimerMax = 300;
+int TimerMax = 1000;
 int Timer = TimerMax-ArrayElements-ArrayElements;
 int Timer2 = 0;
 int ArrayCounter =  0;
 
+int Wacht =0;
+
 
 boolean PompAan = false;
 
+String Message;
 
 void setup(void)
 {
  // start serial port
- Serial.begin(9600);
+ Serial.begin(19200);
 
 
  // initialize the LCD
@@ -132,19 +149,9 @@ void loop(void)
    }
    TempOutdoor = TempOutdoor / ArrayElements;
 
-    Serial.print("  TempPool: ");
-      Serial.print(TempPool);
-      Serial.print("  TempFlowOut: ");
-      Serial.print(TempFlowOut);
-      Serial.print("  TempTopRack: ");
-      Serial.print(TempTopRack);
-      Serial.print("  TempOutdoor: ");
-      Serial.print(TempOutdoor);
-      Serial.print("  Timer: ");
-      Serial.print(Timer);
-      Serial.print("/");
-      Serial.println(TimerMax);
-
+   
+    
+  
 
      
      
@@ -162,97 +169,166 @@ void loop(void)
    //lcd.print("Fridge:");
    //lcd.print(TempFlowOut,1);
 
-
+ if ((!ShortOverRule) && (!LongOverRule))
+ {  
    if (TempPool > MaxTempPool)
    {
-       Serial.println("Info: ZwembadTemp hoger dan 24 graden");
+       //Serial.println("Info: ZwembadTemp hoger dan 24 graden");
        if ( (TempPool - TempOutdoor) > TempDelta )
        {
-           Serial.println("   Pomp aan: Zwembad is 5 graden warmer dan de buitenlucht --> afkoelen");
+           Message="   Pomp aan: Zwembad is 5 graden warmer dan de buitenlucht --> afkoelen";
            PompAan=true;
        }
        else
        {
-           Serial.println("   Pomp uit: Buitentemperatuur te hoog, kan niet afkoelen");
+           Message="   Pomp uit: Buitentemperatuur te hoog, kan niet afkoelen";
            PompAan=true;
        }
    }
    else
    {
-       Serial.println("Info: ZwembadTemp lager dan 24 graden");
+       //Serial.println("Info: ZwembadTemp lager dan 24 graden");
         if ((TempTopRack-TempPool) > TempDelta)
         {
-            Serial.println("   Pomp aan: Verwarming op dak is 5 graden warmer dan het zwembadwater");
+            Message="   Pomp aan: Verwarming op dak is 5 graden warmer dan het zwembadwater";
             PompAan=true;
         }
         else
         {
-            Serial.println("   Pomp uit: Verwarming op dak NIET 5 graden warmer dan het zwembadwater");
+            Message="   Pomp uit: Verwarming op dak NIET 5 graden warmer dan het zwembadwater";
             PompAan=false;
         }
    }
+ }
+
+   if (Wacht == 0)
+   {
+     for(int i = 0; i <= 12; i++)
+     {
+          Serial.println(" ");
+     }
+     Serial.print("TempPool: ");
+     Serial.print(TempPool);
+     Serial.print("  TempFlowOut: ");
+     Serial.print(TempFlowOut);
+     Serial.print("  TempTopRack: ");
+     Serial.print(TempTopRack);
+     Serial.print("  TempOutdoor: ");
+     Serial.print(TempOutdoor);
+     Serial.print("  Timer: ");
+     Serial.print(Timer);
+     Serial.print("/");
+     Serial.println(TimerMax);
+
+     Serial.println(Message);
+     
+     
+     Serial.print("ShortOverRule = ");
+     Serial.print(ShortOverRule);
+     if (ShortOverRule)
+     {
+       Serial.print(" Timer = ");
+       Serial.print( (ShortOverRuleTime - (millis() - ShortOverRuleTimer)) /1000);
+      }
+     Serial.print(" LongOverRule = ");
+     Serial.print(LongOverRule);
+     if (LongOverRule)
+     {
+       Serial.print(" Timer = ");
+       long RemainingTimeSeconds = (LongOverRuleTime - (millis() - LongOverRuleTimer)) / 1000;
+       int Uren = RemainingTimeSeconds  / 3600;
+       int Minuten = (RemainingTimeSeconds - (3600 * Uren)) / 60;
+       int Seconden = RemainingTimeSeconds - (3600 * Uren) - (60 * Minuten);
+       Serial.print( RemainingTimeSeconds);
+       Serial.print(" ");
+       Serial.print(Uren);
+       Serial.print(":");
+       Serial.print(Minuten);
+       Serial.print(":");
+       Serial.println(Seconden);
+       
+      }
+   
+    
+   }
+   Wacht++;
+   if (Wacht == 10)
+   {
+     Wacht = 0;
+   }
   
+    
    //Om te voorkomen dat de pomp continue aan en uit gaat wordt er pas een beslissing genomen als de timer verlopen is
    Timer++;
-   if (Timer>TimerMax)
+   if ((Timer>TimerMax) || (ShortOverRule) || (LongOverRule))
    {
       if (PompAan)
       {
-        Serial.println("Pomp Aangeschakeld");
+        //Serial.println("Pomp Aangeschakeld");
         digitalWrite(PompPin, HIGH);
       }
       else
       {
-        Serial.println("Pomp Uitgeschakeld");
+        //Serial.println("Pomp Uitgeschakeld");
         digitalWrite(PompPin, LOW);
       }
       Timer=0;
    }
       
    //Voor handmatig de pomp aan en uit zetten en een wachtlus te creeeren:
-   for (int i = 0; i < 1000; i++)
-   {
+   
       //Als de knop wordt ingedrukt
       if (digitalRead(ButtonPin)==LOW)
       {
-          delay(500);
-          Serial.println("Button is pressed buiten loop");
-       
-          //Toggle de PompAan variable....
-          if (PompAan)
+          if (buttonActive == false)
           {
-              Serial.println("Pomp Handmatig  Uit");
-              digitalWrite(PompPin, LOW);
-              PompAan = false;
+            buttonActive = true;
+            buttonTimer = millis();
+          }
+
+          if ((millis() - buttonTimer > longPressTime) && (longPressActive == false)) 
+          {
+            longPressActive = true;
+            Serial.println("Knop lang gedrukt");
+            LongOverRule = true;
+            LongOverRuleTimer = millis();
+            PompAan = !PompAan;
+          }
+      }
+      else
+      {
+        if (buttonActive == true)
+        {
+          if (longPressActive == true)
+          {
+            longPressActive = false;
           }
           else
           {
-              Serial.println("Pomp Handmatig Aan");
-              PompAan = true;
-              digitalWrite(PompPin, HIGH);
+            Serial.println("Knop kort gedrukt");
+            ShortOverRule = true;
+            LongOverRule = false;
+            ShortOverRuleTimer = millis();
+            PompAan = !PompAan;
           }
-
-
-          //Houdt deze stand voor de tijd van TImer2 vast      
-          for (Timer2 = 0; Timer2 < 600; Timer2++)
-          {
-             Serial.print("Timer2: ");
-             Serial.println(Timer2);
-       
-             //Als er gedurende de wachttijd toch op de knop gedrukt wordt, dan verlaat de wachtloop
-             if (digitalRead(ButtonPin)==LOW)
-             {
-                Timer2=600;
-                Serial.println("Button is pressed binnen loop");
-             }
-             delay(100);
-             //Zet timer weer op maximaal zodat er een nieuwe beslissing genomen wordt over het aan of uitschakelen van de pomp
-             Timer = TimerMax;
-          }
+          buttonActive = false; 
+        }
       }
+        
+      if ( (millis() - ShortOverRuleTimer) > ShortOverRuleTime) 
+      {
+        ShortOverRule = false;              
+      }
+
+      if ( (millis() - LongOverRuleTimer) > LongOverRuleTime) 
+      {
+        LongOverRule = false;             
+      }
+      //Serial.println(ShortOverRule);
+
       Timer2 = 0; 
+      //delay(1000);
       
-   }
-   delay(500); 
+   
 }
 
